1eb8d0ec50167bcbee1b4aed77bfb23b2d21e88e
Chris James <xcastroj97@gmail.com>
2024-09-25 20:03:46 +0800

    update not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,46 @@
 <template>
-  <div>404</div>
+  <div class="not-found">
+    <h1>404</h1>
+    <h2>Page Not Found</h2>
+    <h3>The page you're looking for doesn't exist</h3>
+    <router-link to="/" class="to-home">Back To Home</router-link>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+@import "../scss/_variables.scss";
+
+.not-found {
+  background-color: #fafafa;
+  text-align: center;
+  padding: 2rem 0;
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  h2 {
+    font-weight: 400;
+    color: #373737;
+  }
+
+  h3 {
+    font-weight: 400;
+    color: #9c9aa6;
+  }
+
+  .to-home {
+    display: inline-block;
+    margin-top: 1.25rem;
+    padding: 1rem;
+    background-color: $green;
+    color: white;
+    border: none;
+    border-radius: 4px;
+
+    &:hover {
+      background: #039545;
+    }
+  }
+}
+</style>
